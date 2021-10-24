0ba9d5474eda551950d1ca3d55df454c8a8e4c93
diler1989 <45041861+diler1989@users.noreply.github.com>
2021-10-24 16:11:17 +0200

    Update error.py

--- error.py
+++ error.py
@@ -5,20 +5,19 @@ import yagmail
 #path to log
 path='/var/log/auth.log'
 
-#adjust error for find
-error = "xxx" 
-
-#adjust email sender
-notify = 'xxx@xxx
+x = "" 
 
 #adjust email reciver
+notify = 'xxx@xxx
+
+#adjust email sender
 glogin = 'xxx'
 gpass = 'xxx'
 
 def mail(error,notify,glogin,gpass):
     yag = yagmail.SMTP(glogin, gpass)
     contents = [
-        "ERROR in /var/log/auth.log: ", error #adjust message
+        "ERROR in /var/log/auth.log: ", error #adjust message body
     ]
     yag.send(notify, 'ERROR in auth.log', contents) #add topic
 
@@ -29,6 +28,6 @@ with open(path, 'r') as f:
         if "error" in line:
             x += line + "\n"
 
-mail(error,notify,glogin,gpass)
+mail(x,notify,glogin,gpass)
 
 
